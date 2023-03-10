f9cad952e3ea7862e8c42a3393426a2931034275
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2023-03-10 19:40:19 +0900

    fix: cliの設定を追加

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,36 @@
 mod read_package_json;
+use clap::Parser;
 use read_package_json::get_scripts;
 
+#[derive(Debug, Parser)]
+#[clap(
+    name = env!("CARGO_PKG_NAME"),
+    version = env!("CARGO_PKG_VERSION"),
+    author = env!("CARGO_PKG_AUTHORS"),
+    about = env!("CARGO_PKG_DESCRIPTION"),
+    arg_required_else_help = true,
+)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(clap::Subcommand, Debug)]
+enum Commands {
+    Run {
+        #[arg(short, long, default_value = "./package.json")]
+        target_path: String,
+    },
+}
+
 fn main() {
-    get_scripts();
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Run { target_path } => {
+            println!("target_path: {:?}", target_path);
+            get_scripts(target_path.to_string());
+        }
+    }
+    // get_scripts();
 }
--- src/read_package_json.rs
+++ src/read_package_json.rs
@@ -1,18 +1,51 @@
 use serde_json::Value;
 use std::fs;
 
-pub fn get_scripts() {
-    let data = get_package_json("package.json".to_string());
+// パッケージのpackage.jsonからscriptsを取得する関数
+pub fn get_scripts(file_path: String) -> Vec<(String, String)> {
+    // パッケージのpackage.jsonを取得
+    let data = get_package_json(file_path);
+    // scriptsオブジェクトを取得
     let scripts = data["scripts"].as_object().unwrap();
 
-    for (key, value) in scripts.iter() {
-        println!("{}: {}", key, value);
-    }
+    // scriptsオブジェクトのキーと値を含むタプルのベクターを作成
+    let script_names: Vec<(String, String)> = scripts
+        .iter()
+        .map(|(k, v)| (k.to_string(), v.as_str().unwrap().to_string()))
+        .collect();
+
+    // script_namesベクターを返す
+    script_names
 }
 
+// パッケージのpackage.jsonをパースする関数
 fn get_package_json(file_path: String) -> Value {
+    // ファイルの内容を文字列として取得
     let contents = fs::read_to_string(&file_path).expect("Something went wrong reading the file");
 
-    fs::read_to_string(&file_path).expect("Something went wrong reading the file");
+    // パースしたJSONを返す
     serde_json::from_str(&contents).unwrap()
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_scripts() {
+        // テスト用のpackage.jsonファイルのパス
+        let file_path = String::from("./test_files/aaa.json");
+
+        // テスト用のscripts
+        let expected_scripts = vec![
+            (String::from("build"), String::from("webpack --config webpack.config.js")),
+            (String::from("start"), String::from("node server.js")),
+            (String::from("test"), String::from("echo \"Error: no test specified\" && exit 1")),
+        ];
+
+        // get_scripts関数で取得したscriptsの一覧
+        let actual_scripts = get_scripts(file_path);
+
+        // 期待値と実際の値が一致するかを検証
+        assert_eq!(expected_scripts, actual_scripts);
+    }
 }
