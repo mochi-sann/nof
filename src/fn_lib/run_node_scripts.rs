43c83e425526da7ffe59f828c9974bf065618792
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2023-03-15 20:17:58 +0900

    fix lint error

--- src/fn_lib/run_node_scripts.rs
+++ src/fn_lib/run_node_scripts.rs
@@ -11,13 +11,6 @@ pub fn run_node_scripts(package_type: NodePackageMannegerType, scripts: String) 
         NodePackageMannegerType::Pnpm => "pnpm",
     };
 
-    let command = format!(
-        "{} {} {}",
-        package_script.to_string(),
-        &"run".to_string(),
-        &scripts.to_string()
-    );
-
     return ReturnCoomad {
         script: package_script.to_string(),
         args: scripts.to_string(),
